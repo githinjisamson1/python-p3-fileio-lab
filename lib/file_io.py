c44213b21a96706ce45c2f0de0acb552717bec5e
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-08 01:39:43 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as text_file:
+        text_file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r", encoding="utf-8") as text_file:
+      return text_file.read()
+  
+  
+# use write_file function.
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+print(read_file(file_name="logfile"))
+
+# Log 1: 5 bananas added
+# Log 2: 3 bananas subtracted
